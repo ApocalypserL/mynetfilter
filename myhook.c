6eda0d390bcecbbb1105f1e43376bafe349efbd5
ApocalypserL <leonhl490@163.com>
2018-05-08 16:04:49 +0800

    I give up. Netlink is not working. Add kernel version modification. A little.

--- myhook.c
+++ myhook.c
@@ -1,6 +1,3 @@
-/* IMPORTANT WARNING
- * THIS VERSION WORKS ONLY ON
- * LINUX KERNEL VERSION LATER THAN 4.13 */
 #ifndef __KERNEL__
 #define __KERNEL__
 #endif
@@ -39,48 +36,17 @@ struct packet_info
 	__u32 daddr;
 };
 
-static struct sock *nlfd;
-
-static void recv_from_user(struct sk_buff *skb)
-{
-
-}
-
-static int send_to_user(struct packet_info *info)
-{
-	struct sk_buff *skb;
-	struct nlmsghdr *nlh;
-	struct packet_info *packet;
-	int size;
-	int ret;
-	unsigned int old_tail;
-
-	size = NLMSG_SPACE(sizeof(*info));
-	skb = alloc_skb(size, GFP_ATOMIC);
-	nlh = nlmsg_put(skb, 0, 0, 0, size - sizeof(*nlh), 0);
-	packet = NLMSG_DATA(nlh);
-	old_tail = skb->tail;
-	memcpy(packet, info, sizeof(struct packet_info));
-
-	packet->saddr = info->saddr;
-	packet->daddr = info->daddr;
-
-	nlh->nlmsg_len = skb->tail - old_tail;
-	NETLINK_CB(skb).dst_group = 0;
-
-	ret = netlink_unicast(nlfd, skb, 0, MSG_DONTWAIT);
-	return ret;
-	
-	if(skb)
-	{
-		kfree_skb(skb);
-		return -1;
-	}
-}
-
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4,1,0)
 static unsigned int hack_skb(void *priv, \
 	       	struct sk_buff *skb, \
 		const struct nf_hook_state *state)
+#else
+static unsigned int hack_skb(const struct nf_hook_ops *ops, \
+		struct sk_buff *skb, \
+		const struct net_device *in, \
+		const struct net_device *out, \
+		int (*okfn)(struct sk_buff *))
+#endif
 {
 	const struct iphdr *iph = ip_hdr(skb);
 	struct packet_info info;
@@ -88,26 +54,8 @@ static unsigned int hack_skb(void *priv, \
 	{
 		info.saddr = iph->saddr;
 		info.saddr = iph->daddr;
-		send_to_user(&info);
 	}
-	/* Test code in early version 
-	printk(KERN_INFO"%u", iph->saddr);
-	printk(KERN_INFO"%u", iph->daddr);
-	switch(iph->protocol)
-	{
-		case IPPROTO_ICMP:
-			printk(KERN_INFO"This is a ICMP.\n");
-			break;
-		case IPPROTO_TCP:
-			printk(KERN_INFO"This is a TCP.\n");
-			break;
-		case IPPROTO_UDP:
-			printk(KERN_INFO"This is a UDP.\n");
-			break;
-		default:
-			printk(KERN_INFO"IDK what it is.\n");
-			break;
-	}*/
+	
 	return NF_ACCEPT;
 }
 
@@ -118,28 +66,22 @@ static struct nf_hook_ops nfho = {
 	.priority	= NF_IP_PRI_FIRST,
 };
 
-static struct netlink_kernel_cfg ncfg = {
-	.input 	= recv_from_user,
-};
-
 static int __init hack_skb_init(void)
 {
-	nlfd = netlink_kernel_create(&init_net, NETLINK_NETFILTER, &ncfg);
-	if(!nlfd)
-	{
-		printk(KERN_ALERT"Initial Module Failed\n");
-		return -1;
-	}
-	return nf_register_net_hook(&init_net, &nfho);	
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4,13,0)
+	return nf_register_net_hook(&init_net, &nfho);
+#else
+	return nf_register_hook(&nfho);
+#endif	
 }
 
 static void __exit hack_skb_exit(void)
 {
-	if(nlfd)
-	{
-		sock_release(nlfd->sk_socket);
-	}
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4,13,0)
 	nf_unregister_net_hook(&init_net, &nfho);
+#else
+	nf_unregister_hook(&nfho);
+#endif
 }
 
 module_init(hack_skb_init);
