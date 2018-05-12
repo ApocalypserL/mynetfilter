eeeb3abe0db027913da86505c30ddc1e78e77021
ApocalypserL <leonhl490@163.com>
2018-05-12 22:42:02 +0800

    Now hookfn can send some packet info to user space.

--- myhook.c
+++ myhook.c
@@ -33,6 +33,12 @@
 #include <net/icmp.h>
 
 #define NETLINK_MYOWN	31
+#define KERN_TO_USER 0
+
+struct
+{
+	__u32 pid;
+}user_proc;
 
 struct packet_info
 {
@@ -40,8 +46,55 @@ struct packet_info
 	__u32 daddr;
 	__u16 sport;
 	__u16 dport;
-	sk_buff_data_t data_h;
-	sk_buff_data_t data_e;
+};
+
+static struct sock *nlfd;
+
+static int send_to_user(struct packet_info info)
+{
+	int ret;
+	int size;
+	struct sk_buff *skb;
+	struct nlmsghdr *nlh;
+	sk_buff_data_t old_tail;
+
+	size = NLMSG_SPACE(sizeof(struct packet_info));
+	skb = alloc_skb(size, GFP_ATOMIC);
+
+	nlh = nlmsg_put(skb, 0, 0, 0, NLMSG_SPACE(sizeof(struct packet_info)) - sizeof(struct nlmsghdr), 0);
+	old_tail = skb->tail;
+	memcpy(NLMSG_DATA(nlh), &info, sizeof(struct packet_info));
+	nlh->nlmsg_len = skb->tail - old_tail;
+
+	NETLINK_CB(skb).portid = 0;
+	NETLINK_CB(skb).dst_group = 0;
+	
+	//printk(KERN_ALERT"[kernel]sending data:%s\n", (char *)NLMSG_DATA((struct nlmsghdr *)skb->data));
+	ret = netlink_unicast(nlfd, skb, user_proc.pid, 1);
+	return ret;
+}
+
+static void recv_from_user(struct sk_buff *skb)
+{
+	struct sk_buff *__skb;
+	struct nlmsghdr *nlh;
+
+	__skb = skb_get(skb);
+	if(__skb->len >= sizeof(struct nlmsghdr))
+	{
+		nlh = nlmsg_hdr(__skb);
+		if((nlh->nlmsg_len >= sizeof(struct nlmsghdr)) && (skb->len >= nlh->nlmsg_len))
+		{
+			user_proc.pid = nlh->nlmsg_pid;
+			printk(KERN_ALERT"[kernel]netlink confirmed, user_proc_pid:%u\n", user_proc.pid);
+		}
+	}
+	
+	kfree_skb(__skb);
+}
+
+struct netlink_kernel_cfg cfg = {
+	.input = recv_from_user,
 };
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4,1,0)
@@ -55,17 +108,21 @@ static unsigned int hack_skb(const struct nf_hook_ops *ops, \
 		const struct net_device *out, \
 		int (*okfn)(struct sk_buff *))
 #endif
-{	
+{
+	int ret = 0;	
 	struct packet_info info;
+	sk_buff_data_t data_h;
+	sk_buff_data_t data_e;
+	__u32 payloadsize = 0;
 	const struct iphdr *iph = ip_hdr(skb);
-
 	if(skb->protocol == IPPROTO_ICMP)
 	{
 		return NF_ACCEPT;
 	}
 		
-	info.data_e = skb->tail;
-	info.data_h = skb->transport_header;
+	printk(KERN_ALERT"Packet hacked!\n");
+	data_e = skb->tail;
+	data_h = skb->transport_header;
 
 	info.saddr = iph->saddr;
 	info.daddr = iph->daddr;
@@ -77,7 +134,8 @@ static unsigned int hack_skb(const struct nf_hook_ops *ops, \
 				const struct tcphdr *tcph = tcp_hdr(skb);
 				info.sport = tcph->source;
 				info.dport = tcph->dest;
-				info.data_h += tcp_hdrlen(skb) + tcp_optlen(skb);
+				data_h += (tcp_hdrlen(skb) + tcp_optlen(skb));
+				payloadsize = data_e - data_h;
 				break;
 			}
 		case IPPROTO_UDP:
@@ -85,16 +143,18 @@ static unsigned int hack_skb(const struct nf_hook_ops *ops, \
 				const struct udphdr *udph = udp_hdr(skb);
 				info.sport = udph->source;
 				info.dport = udph->dest;
-				info.data_h += sizeof(struct udphdr);
+				data_h += sizeof(struct udphdr);
+				payloadsize = data_e - data_h;
 				break;
 			}
 		default:
-			break;
+			return NF_ACCEPT;
 	}
-
-	
-	printk(KERN_ALERT"%u\n", info.saddr);
-	printk(KERN_ALERT"%u\n", info.daddr);
+	if((user_proc.pid) > 0)
+	{
+		ret = send_to_user(info);
+	}
+	//printk(KERN_ALERT"%u\n", info.daddr);
 	return NF_ACCEPT;
 }
 
@@ -107,6 +167,17 @@ static struct nf_hook_ops nfho = {
 
 static int __init hack_skb_init(void)
 {
+	user_proc.pid = 0;
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,10,0)
+	nlfd = netlink_kernel_create(&init_net, NETLINK_MYOWN, &cfg);
+#else
+	nlfd = netlink_kernel_create(&init_net, NETLINK_MYOWN, &cfg);
+#endif
+	if(!nlfd)
+	{
+		printk(KERN_ALERT"create netlink failed.\n");
+		return -1;
+	}
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4,13,0)
 	return nf_register_net_hook(&init_net, &nfho);
 #else
@@ -116,6 +187,8 @@ static int __init hack_skb_init(void)
 
 static void __exit hack_skb_exit(void)
 {
+	sock_release(nlfd->sk_socket);
+	printk(KERN_ALERT"delete netlink.\n");
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4,13,0)
 	nf_unregister_net_hook(&init_net, &nfho);
 #else
